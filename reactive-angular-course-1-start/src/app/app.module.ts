7f450052add8a728f6af4d41b81caaf0f14ddfa5
FiniteLooper <chris.m.barr@gmail.com>
2024-04-20 11:18:19 -0400

    Completed section 2 - "Stateless Observables Service"

--- reactive-angular-course-1-start/src/app/app.module.ts
+++ reactive-angular-course-1-start/src/app/app.module.ts
@@ -33,6 +33,7 @@ import {SafeUrlPipe} from './common/safe-url.pipe';
 import {MessagesComponent} from './messages/messages.component';
 import {SearchLessonsComponent} from './search-lessons/search-lessons.component';
 import { LoadingComponent } from './loading/loading.component';
+import { CoursesCardListComponent } from './courses-card-list/courses-card-list.component';
 
 @NgModule({
   declarations: [
@@ -41,6 +42,7 @@ import { LoadingComponent } from './loading/loading.component';
     AboutComponent,
     CourseComponent,
     CourseDialogComponent,
+    CoursesCardListComponent,
     LoginComponent,
     LessonComponent,
     SafeUrlPipe,
---
+++ reactive-angular-course-1-start/src/app/courses-card-list/courses-card-list.component.ts
@@ -0,0 +1,41 @@
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Course } from "../model/course";
+import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
+import { CourseDialogComponent } from "../course-dialog/course-dialog.component";
+import { filter, tap } from "rxjs/operators";
+
+@Component({
+  selector: "courses-card-list",
+  templateUrl: "./courses-card-list.component.html",
+  styleUrl: "./courses-card-list.component.css",
+})
+export class CoursesCardListComponent implements OnInit {
+  @Input() courses: Course[] = [];
+
+  @Output() private coursesChanged = new EventEmitter();
+
+  constructor(private readonly dialog: MatDialog) {}
+
+  ngOnInit(): void {}
+
+  editCourse(course: Course) {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = "400px";
+    dialogConfig.data = course;
+
+    const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
+
+    dialogRef
+      .afterClosed()
+      .pipe(
+        filter((val) => !!val),
+        tap(() => {
+          this.coursesChanged.emit();
+        })
+      )
+      .subscribe();
+  }
+}
