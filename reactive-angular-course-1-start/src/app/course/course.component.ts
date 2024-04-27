d29c07ac2d2359fdf0cc8c2517bf8ca5b26b787b
FiniteLooper <chris.m.barr@gmail.com>
2024-04-27 10:14:03 -0400

    Finished section 6: The Single Data Observable Pattern

--- reactive-angular-course-1-start/src/app/course/course.component.ts
+++ reactive-angular-course-1-start/src/app/course/course.component.ts
@@ -1,19 +1,35 @@
-import { Component, OnInit } from "@angular/core";
+import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { Course } from "../model/course";
 import { Lesson } from "../model/lesson";
+import { Observable, combineLatest } from "rxjs";
+import { CoursesService } from "../services/courses.service";
+import { map, startWith, tap } from "rxjs/operators";
+
+interface ICourseData {
+  course: Course;
+  lessons: Lesson[];
+}
 
 @Component({
   selector: "course",
   templateUrl: "./course.component.html",
   styleUrls: ["./course.component.css"],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CourseComponent implements OnInit {
-  course: Course;
+  data$: Observable<ICourseData>;
 
-  lessons: Lesson[];
+  constructor(private readonly route: ActivatedRoute, private readonly coursesService: CoursesService) {}
 
-  constructor(private route: ActivatedRoute) {}
+  ngOnInit() {
+    const courseId = parseInt(this.route.snapshot.paramMap.get("courseId"));
+    const course$ = this.coursesService.loadCourseById(courseId).pipe(startWith({}))
+    const lessons$ = this.coursesService.loadAllCourseLessons(courseId).pipe(startWith([]))
 
-  ngOnInit() {}
+    this.data$ = combineLatest([course$, lessons$]).pipe(
+      map(([course, lessons]) => ({ course, lessons })),
+      tap(console.log)
+    );
+  }
 }
--- reactive-angular-course-1-start/src/app/services/courses.service.ts
+++ reactive-angular-course-1-start/src/app/services/courses.service.ts
@@ -16,8 +16,26 @@ export class CoursesService {
     );
   }
 
+  loadCourseById(courseId: number): Observable<Course> {
+    return this.http.get<Course>(`/api/courses/${courseId}`).pipe(shareReplay());
+  }
+
   saveCourse(courseId: string, changes: Partial<Course>): Observable<any> {
     return this.http.put(`/api/courses/${courseId}`, changes).pipe(shareReplay());
+  }
+
+  loadAllCourseLessons(courseId: number): Observable<Lesson[]> {
+    return this.http
+      .get<Lesson[]>("/api/lessons", {
+        params: {
+          pageSize: "10000",
+          courseId: courseId.toString()
+        },
+      })
+      .pipe(
+        map((res) => res["payload"]),
+        shareReplay()
+      );
   }
 
   searchLessons(search: string): Observable<Lesson[]> {
